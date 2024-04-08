262b2c26bc267998df95b688b4ab21f953ffa146
jack <giacomo.bertini@tasgroup.eu>
2024-04-08 10:15:57 +0200

    commit sql

--- BankAppSpringMVC/sql/create_script.sql
+++ BankAppSpringMVC/sql/create_script.sql
@@ -25,8 +25,8 @@ create table Transazione (
 	importo double not null,
 	tipo varchar not null,
 	constraint p_idtra primary key(id_transazione),
-	constraint f_iddes foreign key(id_conto) references Conto(id_conto),
-	constraint f_idsor foreign key(id_conto) references Conto(id_conto)
+	constraint f_idsor foreign key(id_conto) references Conto(id_conto),
+	constraint f_iddes foreign key(id_conto) references Conto(id_conto)
 );
 
 create table Ruolo (
