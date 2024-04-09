f9afb4e2662ecb743779e46e0f2179ad224ab1c8
jack <giacomo.bertini@tasgroup.eu>
2024-04-09 20:52:07 +0200

    commit serale jack

--- BankAppSpringMVC/src/main/java/com/tasgroup/bankapp/businesscomponent/dao/RuoloDAO.java
+++ BankAppSpringMVC/src/main/java/com/tasgroup/bankapp/businesscomponent/dao/RuoloDAO.java
@@ -1,6 +1,7 @@
 package com.tasgroup.bankapp.businesscomponent.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.tasgroup.bankapp.businesscomponent.model.Ruolo;
@@ -16,6 +17,10 @@ public class RuoloDAO implements DAOConstants{
 	public int create(Ruolo r) {
 		return jdbct.update(INSERT_RUOLO,
 				new Object[] {r.getRuolo()});
+	}
+	
+	public Ruolo findById(long id) {
+		return jdbct.queryForObject(SELECT_RUOLO_BYID, new Object[] {id}, new BeanPropertyRowMapper<Ruolo>(Ruolo.class));
 	}
 	
 	public int update(Ruolo r) {
--- BankAppSpringMVC/src/main/java/com/tasgroup/bankapp/controller/RuoloController.java
+++ BankAppSpringMVC/src/main/java/com/tasgroup/bankapp/controller/RuoloController.java
@@ -1,5 +1,56 @@
 package com.tasgroup.bankapp.controller;
 
-public class RuoloController {
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tasgroup.bankapp.businesscomponent.dao.RuoloDAO;
+import com.tasgroup.bankapp.businesscomponent.model.Ruolo;
+
+@Controller
+public class RuoloController {
+	@Autowired
+	RuoloDAO rDAO;
+	
+	@RequestMapping("/ruoloForm")
+	public String visualizzaForm(Model m) {
+		m.addAttribute("ruolo", new Ruolo());
+		return "ruoloFormPage";
+	}
+	
+	@RequestMapping(value= "/salvaRuolo", method= RequestMethod.POST)
+	public ModelAndView createRuolo(@Valid @ModelAttribute("ruolo") Ruolo ruolo, BindingResult br) {
+		if(br.hasErrors()) {
+			return new ModelAndView("ruoloFormPage","command",ruolo);
+		} else {
+			rDAO.create(ruolo);
+			return new ModelAndView("redirect:/ruoloReport");
+		}
+	}
+	
+	@RequestMapping("/trovaRuolo/{id}")
+	public ModelAndView trovaRuolo(@PathVariable long id) {
+		Ruolo ruolo= rDAO.findById(id);
+		return new ModelAndView("trovaRuoloPage","command",ruolo);
+	}
+	
+	@RequestMapping(value= "/modifica", method= RequestMethod.POST)
+	public ModelAndView modificaRuolo(Ruolo r) {
+		rDAO.update(r);
+		return new ModelAndView("redirect:/ruoloReport");
+	}
+	
+	@RequestMapping("/cancella/{id}")
+	public ModelAndView cancellaRuolo(@PathVariable long id) {
+		rDAO.delete(id);
+		return new ModelAndView("redirect:/ruoloReport");
+	}
 }
